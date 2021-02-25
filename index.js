1fa33291b93f050185e33762439ee15a90035a61
soojene <73275204+soojene@users.noreply.github.com>
2021-02-25 20:38:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const express = require('express')
-const app = express()
+// 노드 모듈에서 import 하고,
+//import한 것을 실행execute한 app만들고, 
+//만든 app이 포트를 listen하게 하고 함수도 실행시키게 함
+
+const express = require('express') 
+const app = express() 
 
 const PORT = 4000;
 
