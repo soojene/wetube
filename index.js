322486a23f1cab0ce7f8fbe85fd4ef2bdcc1f561
soojene <73275204+soojene@users.noreply.github.com>
2021-03-01 10:24:22 +0900

    install babel, core-js3, nodemon

--- index.js
+++ index.js
@@ -2,13 +2,19 @@
 //import한 것을 실행execute한 app만들고, 
 //만든 app이 포트를 listen하게 하고 함수도 실행시키게 함
 
-const express = require('express') 
+import "core-js";
+import express from "express";
 const app = express() 
 
 const PORT = 4000;
 
-function handleLinstening(){
-    console.log(`Listening on: http://localhost:${PORT}`);
-}
+const handleLinstening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
+const handleHome = (req, res) => res.send("hello from home");
+// console.log(req); 콘솔로 request의 함수들을 확인 가능
+
+const handleProfile = (req, res) => res.send("you are my profile page");
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
 app.listen(PORT, handleLinstening);
