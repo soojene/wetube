d462f5c949ba3bf60984020ad51405102df07f2a
soojene <73275204+soojene@users.noreply.github.com>
2021-04-11 22:07:46 +0900

    setup webpack

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use((req, res, next) => {
 // app.use(helmet());
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
